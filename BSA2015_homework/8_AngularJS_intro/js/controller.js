d453a2295fff401bbec45401c52a93b41a5cda1f
Bieliakov <belyakov_sergei@ukr.net>
2015-07-10 22:37:36 +0300

    update controller.js

--- BSA2015_homework/8_AngularJS_intro/js/controller.js
+++ BSA2015_homework/8_AngularJS_intro/js/controller.js
@@ -1,15 +1,48 @@
 
 angular.module('app', [])
-    .controller('firstController', function($scope) {
-        $scope.people = [{name: 'John', 'home city': 'New York', age: '20'},
-                        {name: 'Mark', 'home city': 'Kyiv', age: '23'}];
+    .controller('usersController', function($scope) {
+
+
+
+        $scope.people = [{name: 'John', home_city: 'New York', age: '20', avatar: 'jimKerry.gif'},
+                        {name: 'Mark', home_city: 'Kyiv', age: '23', avatar: 'cat.jpg'}];
         $scope.addPerson = function (){
-            $scope.people.push({name: 'name', 'home city': 'New York', age: '20'});
+            $scope.people.push({name: 'name',
+                                home_city: 'New York',
+                                age: Math.floor(Math.random()*100),
+                                avatar: 'raduga.jpeg' });
+            // issue
+            for (var i = 0; i< $scope.people.length - 1; i++) {
+                if ($scope.people[i].name === $scope.people[$scope.people.length - 1].name) {
+                    $scope.people.pop()
+                }
+            }
+        };
+        $scope.removePerson = function (index){
+            $scope.people.splice(index,1);
+        };
+
+        $scope.showPeopleList = true;
+
+        $scope.toggleViewPeople = function(){
+            $scope.showPeopleList = !$scope.showPeopleList;
+        };
+    })
+    .controller('productsController', function($scope){
+        $scope.products = [{ title: 'Teddy bear', price: '50$'},{title: 'Cat', price: '80$'}]
+
+        $scope.addProduct = function (){
+            $scope.products.push({title: 'Dog', price: Math.floor(Math.random()*1000) });
 
         };
-        $scope.removePerson = function (){
-            $scope.people.pop();
+        $scope.removeProduct = function (index){
+            $scope.products.splice(index,1);
+        };
 
+        $scope.showProductsList = true;
+
+        $scope.toggleViewProducts = function(){
+            $scope.showProductsList = !$scope.showProductsList;
         };
     });
 
--- BSA2015_homework/MVC_work6/controllers.js
+++ BSA2015_homework/MVC_work6/controllers.js
@@ -15,17 +15,33 @@ function Controller(obj_controller){
 
         that.checkChanges();
         
+        // насколько я понял, это и есть делегирование. Т.е. когда на родительский элемент вешается
+        // слушатель, и потом, в зависимости от дочернего элемента, на который был произведён клик - 
+        // уже вызывается соответствующая функция для обработки евента для конкретного дочернего элемента.
+        
         $('#' + that.elementId).delegate('button', 'click', function(event){
             event.stopImmediatePropagation();
             //console.log($(event.target).attr('id'));
-
             for (var prop in that.clickHandlers){
                 if (prop === '#' + $(event.target).attr('id')) {
-                    //console.log(that.clickHandlers[prop]); 
-                    that[that.clickHandlers[prop]](); // invokes updateExams function in our example
+                    that[that.clickHandlers[prop]](); // вызывется updateExams функция
                 }                
             }
         });
+        
+        // кусок кода из первого коммита. Сказали переделать через делегирование.
+        
+        /*
+        for (var prop in that.clickHandlers){
+-            if (that.clickHandlers[prop] in that){
+-                $(prop).on('click', function(){ 
+-                    that.updateExams();
+-                });
+-            };
+-        };
+        */
+        
+        
     })();
 }
 
@@ -33,8 +49,10 @@ Controller.prototype = {
     checkChanges: function(){
         setInterval($.proxy(function(){
             if (this.model.changed) {
+                // уверен, что не лучший вариант выполнения задачи, но работает :)
                 $('#' + this.elementId).empty();
                 $('#' + this.elementId).append(this.render());
+                $('#' + this.elementId).append('<span>examsTaken: ' + this.model.examsTaken + '</span>');
                 //console.log(this.model.changed);
                 this.model.changed = false;
             }
@@ -48,8 +66,7 @@ var StudentController = new Controller({
     model: Student,
     elementId: 'student-container',
     render: function(){
-        return '<span>' + this.model.name + '</span><button id="student-exams-button">Increase exams taken</button>' +
-        '<span>' + this.model.examsTaken + '</span>'; // added for visualization
+        return '<span>' + this.model.name + '</span><button id="student-exams-button">Increase exams taken</button>';
     },
     clickHandlers: {
         '#student-exams-button': 'updateExams'
