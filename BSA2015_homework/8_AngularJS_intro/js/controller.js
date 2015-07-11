e1fcd827a96ecba8f626efa2d14ebe798da74bf1
Bieliakov <belyakov_sergei@ukr.net>
2015-07-11 20:26:37 +0300

    update hw8

--- BSA2015_homework/8_AngularJS_intro/js/controller.js
+++ BSA2015_homework/8_AngularJS_intro/js/controller.js
@@ -11,12 +11,18 @@ angular.module('app', [])
                                 home_city: 'New York',
                                 age: Math.floor(Math.random()*100),
                                 avatar: 'raduga.jpeg' });
-            // issue
-            for (var i = 0; i< $scope.people.length - 1; i++) {
+
+            // don't look into commented code :)
+            /*
+            if ($scope.people[$scope.people.length - 2].name === $scope.people[$scope.people.length - 1].name) {
+                $scope.people.pop()
+            }
+            */
+            /*for (var i = 0; i< $scope.people.length - 1; i++) {
                 if ($scope.people[i].name === $scope.people[$scope.people.length - 1].name) {
                     $scope.people.pop()
                 }
-            }
+            }*/
         };
         $scope.removePerson = function (index){
             $scope.people.splice(index,1);
@@ -32,7 +38,7 @@ angular.module('app', [])
         $scope.products = [{ title: 'Teddy bear', price: '50$'},{title: 'Cat', price: '80$'}]
 
         $scope.addProduct = function (){
-            $scope.products.push({title: 'Dog', price: Math.floor(Math.random()*1000) });
+            $scope.products.push({title: 'Dog', price: Math.floor(Math.random()*50) + '$' });
 
         };
         $scope.removeProduct = function (index){
