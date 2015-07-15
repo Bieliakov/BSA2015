69ef44b65f7e8ed66e1d7326e38deeb2f02a899b
Bieliakov <belyakov_sergei@ukr.net>
2015-07-15 15:49:53 +0300

    done hw9

---
+++ BSA2015_homework/9_AngularJS_dive_deeply/scripts/service_$resource.js
@@ -0,0 +1,21 @@
+(function() {
+    angular
+        .module('app')
+        .service('Photos', Photos);
+
+    function Photos($resource){
+        this.getPhotos = function () {
+            var firstSeveralPhotos = [];
+
+            $resource('http://jsonplaceholder.typicode.com/photos').query(function (response) {
+                for (var prop in response) {
+                    firstSeveralPhotos.push(response[prop]);
+                    if (response[prop].id === 5) {
+                        break;
+                    }
+                }
+            });
+            return firstSeveralPhotos;
+        }; // end getPhotos function
+    } // end 'Photos' service
+})();
