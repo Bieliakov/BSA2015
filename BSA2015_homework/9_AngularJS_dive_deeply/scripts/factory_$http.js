cbae45ae4bf440df5d805d3c744388aa120e8c4d
Bieliakov <belyakov_sergei@ukr.net>
2015-07-16 11:29:37 +0300

    slight loop change in service_$resource

--- BSA2015_homework/9_AngularJS_dive_deeply/scripts/factory_$http.js
+++ BSA2015_homework/9_AngularJS_dive_deeply/scripts/factory_$http.js
@@ -1,27 +1,27 @@
-(function() {
-    angular
-        .module('app')
-        .factory('Photos', Photos);
-
-    function Photos($http){
-        return {
-            getPhotos: function () {
-                var firstSeveralPhotos = [];
-                $http.get('http://jsonplaceholder.typicode.com/photos')
-                    .then(function (response) {
-                        var length = response.data.length;
-                        for (var i = 0; i < length; i++) {
-                            firstSeveralPhotos.push(response.data[i]);
-                            //console.log(response.data[i])
-                            if (response.data[i].id === 5)
-                                break;
-                        }
-                        //console.log(firstSeveralPhotos)
-                    }).catch(function (err) {
-                        console.log(err);
-                    });
-                return firstSeveralPhotos;
-            }
-        }
-    }
-})();
+//(function() {
+//    angular
+//        .module('app')
+//        .factory('Photos', Photos);
+//
+//    function Photos($http){
+//        return {
+//            getPhotos: function () {
+//                var firstSeveralPhotos = [];
+//                $http.get('http://jsonplaceholder.typicode.com/photos')
+//                    .then(function (response) {
+//                        var length = response.data.length;
+//                        for (var i = 0; i < length; i++) {
+//                            firstSeveralPhotos.push(response.data[i]);
+//                            //console.log(response.data[i])
+//                            if (response.data[i].id === 5)
+//                                break;
+//                        }
+//                        //console.log(firstSeveralPhotos)
+//                    }).catch(function (err) {
+//                        console.log(err);
+//                    });
+//                return firstSeveralPhotos;
+//            }
+//        }
+//    }
+//})();
--- BSA2015_homework/9_AngularJS_dive_deeply/scripts/service_$resource.js
+++ BSA2015_homework/9_AngularJS_dive_deeply/scripts/service_$resource.js
@@ -1,22 +1,23 @@
-//(function() {
-//    angular
-//        .module('app')
-//        .service('Photos', Photos);
-//
-//    function Photos($resource){
-//        this.getPhotos = function () {
-//            var firstSeveralPhotos = [];
-//
-//            $resource('http://jsonplaceholder.typicode.com/photos').query(function (response) {
-//                for (var prop in response) {
-//                    firstSeveralPhotos.push(response[prop]);
-//                    if (response[prop].id === 5) {
-//                        break;
-//                    }
-//                }
-//            });
-//            return firstSeveralPhotos;
-//        }; // end getPhotos function
-//
-//    } // end 'Photos' service
-//})();
+(function() {
+    angular
+        .module('app')
+        .service('Photos', Photos);
+
+    function Photos($resource){
+        this.getPhotos = function () {
+            var firstSeveralPhotos = [];
+
+            $resource('http://jsonplaceholder.typicode.com/photos').query(function (response) {
+                var length = response.length;
+                for (var i = 0; i < length; i++) {
+                    firstSeveralPhotos.push(response[i]);
+                    if (response[i].id === 5) {
+                        break;
+                    }
+                }
+            });
+            return firstSeveralPhotos;
+        }; // end getPhotos function
+
+    } // end 'Photos' service
+})();
